45c8e9edeba472049c63554eede2abf54ded8a94
rodrigo-dev <rodrigo.justino@voxtec.com.br>
2025-05-07 19:48:58 -0300

    feat: create migration harvests

---
+++ database/migrations/2025_05_02_225524_create_harvests_table.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('harvests', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuid('batche_id');
+            $table->float('total_weight');
+            $table->float('price_per_kg');
+            $table->float('total_revenue');
+            $table->date('harvest_date');
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('batch_id')->references('id')->on('batches')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('harvests');
+    }
+};

2ff3e344d4bd49cf5e72c020818abd82aaf7729b
rodrigojustinodevs <rodrigojustino.devs@gmail.com>
2025-12-27 16:36:08 -0300

    feat: adiciona documentação Swagger/OpenAPI

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('login', [AuthController::class, 'authenticateUser']);
 
+
 Route::group(['middleware' => ['auth:api']], function (): void {
     Route::get('/ping', fn (): string => 'pong');
 });
